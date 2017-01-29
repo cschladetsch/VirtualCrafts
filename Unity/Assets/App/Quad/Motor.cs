76db3fa0571df5f446b8aeea3203ab7cc261fa6c
Christian <christian@twobulls.com>
2017-01-29 20:18:43 +1100

    Created

--- Unity/Assets/App/Quad/Motor.cs
+++ Unity/Assets/App/Quad/Motor.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 using UnityEngine.Assertions;
 using Random = UnityEngine.Random;
 
+using ChaosCult.SceneLabels;
 using App.Math;
 using App.Utils;
 
@@ -16,8 +17,40 @@ namespace App
 {
 	public class Motor : MonoBehaviour 
 	{
+		public enum ESpin
+		{
+			CW, CCW,
+		}
+
+		public float RevsPerMinute;		// revolutions per minute
+		public float LiftFactor;			// how much lift motor generates per rev/min
+		public ESpin Spin;
+		public Vector3 Force;
+
+		public float LiftForce
+		{
+			get
+			{
+				return ForceDir*RevsPerMinute*LiftFactor*ForceDir;
+			}
+		}
+
+		public Vector3 ForceDir
+		{
+			get
+			{
+				
+			}
+		}
+
+		float ForceDir()
+		{
+			return Spin == ESpin.CW ? 1 : -1;
+		}
+
 		private void Awake()
 		{
+
 		}
 
 		private void Start()
@@ -30,6 +63,11 @@ namespace App
 
 		private void FixedUpdate()
 		{
+		}
+
+		private void OnDrawGizmos()
+		{
+			LabelsAccess.DrawLabel(transform.position, gameObject.name, null);
 		}
 	}
 }
