e9d364dff8331c23c48fd0e6ddff0447698c4395
Christian <christian@twobulls.com>
2017-02-06 00:11:29 +1100

    Revert "unfuck move"
    This reverts commit d8afb65ca9d36cda8a6302f9de7dc38697e46715.

---
+++ Transmitter-RawTcp/Unity/Assets/App/Network/Ticker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace App
+{
+    public class Ticker : MonoBehaviour 
+	{
+		public float Interval = 0.100f;
+
+		private float _timer;
+
+		void Awake()
+		{
+			_timer = Interval;
+		}
+
+		private void Update()
+		{
+			_timer -= Time.deltaTime;
+			if (_timer < 0)
+			{
+				Debug.Log("Tick: " + Time.time);
+				_timer = Interval;
+			}
+		}
+	}
+}
+
