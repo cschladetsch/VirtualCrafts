9ba4beb86541e621e95bf479a5cb18dcba5ed0a0
Christian <christian@twobulls.com>
2017-02-05 18:28:16 +1100

    Failed at using System.Net.* with Unity

---
+++ Transmitter-RawTcp/Unity/Assets/App/Math/Static.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using Random = UnityEngine.Random;
+
+using App.Utils;
+
+using UniRx;
+
+namespace App.Math
+{
+	public static class Static 
+	{
+	}
+}
+
