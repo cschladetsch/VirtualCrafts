0febaaad6bbab77dbd77ffa87f24159fefc6def9
Christian <christian@twobulls.com>
2017-02-07 20:49:28 +1100

    Added smart camera controller using PIDs.

---
+++ Crafts/Unity/Assets/App/FollowingCamera.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+
+using App.Math;
+
+namespace App
+{
+	/// <summary>
+	/// A system that follows a target from behind and at a distance up 
+	/// </summary>
+    public class FollowingCamera : MonoBehaviour 
+	{
+		public GameObject Target;	// what to keep on screen
+		public float LookAhead;		// how far to look ahead of object
+		public float LagBehind;		// how far to stay behind it
+		public float Height;		// how high above it
+		public Transform Controlled;
+
+		public Vector3 MovePidFactors;
+		public PidVector3Controller MoveController = new PidVector3Controller();
+
+		// not used yet: maybe later will smooth the camera orientation as well
+		// public Vector4 OrientationPidFactors;
+		// public PidQuaternionController OrientationController;
+
+		// not used yet: maybe later will pass 
+		// public ButterworthFilter FilteredPosition;
+
+		private void Update()
+		{
+			MoveController.SetPid(MovePidFactors);
+
+			var dt = Time.deltaTime;
+			var tp = Target.transform.position; 
+			var tf = Target.transform.forward;
+			var lookAt = tp + tf*LookAhead;
+			var belowCam = tp - tf*LagBehind;
+			var desired = belowCam + Vector3.up*Height;
+
+			var delta = MoveController.Calculate(desired, Controlled.position, dt);
+			Controlled.position += delta;
+
+			Controlled.LookAt(lookAt, Vector3.up);	// TODO: PidQuaternionController
+		}
+	}
+}
+
--- Crafts/Unity/Assets/App/Math/PidVector3Controller.cs
+++ Crafts/Unity/Assets/App/Math/PidVector3Controller.cs
@@ -5,12 +5,12 @@ namespace App.Math
     public class PidVector3Controller
 	{
 		// target location
-		public Vector3 SetPoint;
+		public Vector3 Target;
 
 		// pid co-efficients
-		public float Kp = 0.5f;
-		public float Ki = 0.05f;
-		public float Kd = 0.1f;
+		public float P = 0.5f;
+		public float I = 0.05f;
+		public float D = 0.1f;
 
 		public PidVector3Controller()
 		{
@@ -19,41 +19,78 @@ namespace App.Math
 
 		public PidVector3Controller(float p, float i, float d)
 		{
-			Kp = p;
-			Ki = i;
-			Kd = d;
+			P = p;
+			I = i;
+			D = d;
 
 			CreateControllers();
 		}
 
-		public Vector3 Calculate(Vector3 pv, float dt)
+		/// <summary>
+		/// Calculate the absolute delta to add to the current control signal
+		/// given current control signal and a delta-time since last call
+		/// <param name="current">current value</param>
+		/// <param name="dt">delta time</param>
+		/// <returns>an absolute change to add to the current value passed in. does not require dt scaling</returns>
+		public Vector3 Calculate(Vector3 current, float dt)
 		{
-			return Calculate(SetPoint, pv, dt);
+			return Calculate(Target, current, dt);
 		}
 
 		/// <summary>
 		/// PID controller for a Vector3.
 		/// The result is a delta to be added to the process value.
 		/// </summary>
-		/// <param name="setPoint"></param>
-		/// <param name="pv"></param>
+		/// <param name="target"></param>
+		/// <param name="current">the current position</param>
 		/// <param name="dt"></param>
 		/// <returns></returns>
-		public Vector3 Calculate(Vector3 setPoint, Vector3 pv, float dt)
+		public Vector3 Calculate(Vector3 target, Vector3 current, float dt)
 		{
-			float x = _controllers[0].Calculate(setPoint.x, pv.x, dt);
-			float y = _controllers[1].Calculate(setPoint.y, pv.y, dt);
-			float z = _controllers[2].Calculate(setPoint.z, pv.z, dt);
+			float x = _controllers[0].Calculate(target.x, current.x, dt);
+			float y = _controllers[1].Calculate(target.y, current.y, dt);
+			float z = _controllers[2].Calculate(target.z, current.z, dt);
 
 			return new Vector3(x,y,z);
+		}
+
+		/// <summary>
+		/// Set all the PID factors for all three controllers at once.
+		/// Use SetPid[X,Y,Z]() to set the pid factors for each axes separately.
+		/// </summary>
+		/// <param name="pid">the pid factors to use for each of x,y,z axes</param>
+		public void SetPid(Vector3 pid)
+		{
+			for (var n = 0; n < 3; ++n)
+			{
+				var c = _controllers[n];
+				c.P = pid.x;
+				c.I = pid.y;
+				c.D = pid.z;
+			}
+		}
+
+		public void SetPidX(Vector3 pid)
+		{
+			_controllers[0].SetPid(pid);
+		}
+
+		public void SetPidY(Vector3 pid)
+		{
+			_controllers[1].SetPid(pid);
+		}
+
+		public void SetPidZ(Vector3 pid)
+		{
+			_controllers[2].SetPid(pid);
 		}
 
 		void CreateControllers()
 		{
 			_controllers = new[] {
-				new PidScalarController(Kp, Ki, Kd),
-				new PidScalarController(Kp, Ki, Kd),
-				new PidScalarController(Kp, Ki, Kd)
+				new PidScalarController(P, I, D),
+				new PidScalarController(P, I, D),
+				new PidScalarController(P, I, D)
 			};
 		}
 
